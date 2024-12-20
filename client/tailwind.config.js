9a8d41f88fc28b905f0e6dfa4f5d194195d168da
Anubrata-01 <anubratachanda36@gmail.com>
2024-12-20 15:47:08 +0530

    modified

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,16 +1,16 @@
-/* eslint-disable no-undef */
-const { nextui } = require("@nextui-org/react");
+
+import { nextui } from "@nextui-org/react";
 
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   darkMode: "class",
   plugins: [nextui()],
-}
+};
 
