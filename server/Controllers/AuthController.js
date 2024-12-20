eaab51ed5e3a5e53cfed977c0b486fdf90e1c5b1
Anubrata-01 <anubratachanda36@gmail.com>
2024-12-20 17:22:55 +0530

    updated

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -79,7 +79,7 @@ export const SignUp = async (req, res, next) => {
           maxAge: REFRESH_TOKEN_EXPIRY,
       });
 
-        return res.status(201).send({
+        return res.status(201).json({
             message: "User created successfully",
             user: {
                 username: newUser.username,
@@ -89,7 +89,7 @@ export const SignUp = async (req, res, next) => {
         });
     } catch (error) {
         console.error(error);
-        return res.status(500).send("Internal server error");
+        return res.status(500).json({ message: "Failed to sign in." });
     }
 };
 
@@ -144,7 +144,7 @@ export const SignIn = async (req, res, next) => {
           maxAge: REFRESH_TOKEN_EXPIRY,
       });
 
-        return res.status(200).send({
+        return res.status(200).json({
             message: "User signed in successfully",
             user: {
                 username: user.username,
@@ -154,7 +154,7 @@ export const SignIn = async (req, res, next) => {
         });
     } catch (error) {
         console.error(error);
-        return res.status(500).send("Internal server error");
+        return res.status(500).json({ message: "Failed to sign in." });
     }
 };
 
