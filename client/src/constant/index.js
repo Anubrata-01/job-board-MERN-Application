36af392312cc99a62ac92e24d7087067ce3d5f3e
Anubrata-01 <anubratachanda36@gmail.com>
2024-12-20 16:20:22 +0530

    updated

--- client/src/constant/index.js
+++ client/src/constant/index.js
@@ -1,4 +1,10 @@
-export const HOST = "http://localhost:5000"
+// export const ProdUrl="https://job-board-mern-application-backend.onrender.com"
+// export const DevUrl="http://localhost:5000"
+// export const HOST = "http://localhost:5000"
+export const ProdUrl = "https://job-board-mern-application-backend.onrender.com";
+export const DevUrl = "http://localhost:5000";
+export const HOST = import.meta.env.PROD ? ProdUrl : DevUrl;
+
 export const AUTH_ROUTES=`${HOST}/api/auth`;
 export const SIGNUP_URL=`${AUTH_ROUTES}/signup`
 export const SIGNIN_URL=`${AUTH_ROUTES}/signin`
--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(cors({
 app.use("/api/auth",router)
   console.log(`Running in ${process.env.NODE_ENV} mode`);
   connectToDB()
-  const PORT = process.env.PORT || 7000;
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 //   const {filename}=import.meta
 // console.log(filename)
